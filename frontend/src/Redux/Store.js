6c591d6682dc25f6bd135b104ee5550c78c1ab01
Ashwani-Sahu <sahua5979@gmail.com>
2021-07-20 21:39:43 +0530

    basic structure added

---
+++ frontend/src/Redux/Store.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, combineReducers, createStore } from "redux"
+import { compose } from "redux"
+import thunk from "redux-thunk"
+
+const rootreducer = combineReducers({
+
+})
+
+const store = createStore(rootreducer, 
+    compose(applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    ))
+
+export default store;
---
+++ frontend/src/Routing/Routes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Routes() {
+    return (
+        <div>
+            Routes needed here      
+        </div>
+    )
+}
+
+export default Routes
